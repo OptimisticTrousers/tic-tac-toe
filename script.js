adb7a0720102886e6eb9dd806aeb18853a8384af
OptimisticTrousers <tony.isern@yahoo.com>
2022-05-28 14:54:10 -0400

    Adding styling and creating an HTML grid for the game

--- script.js
+++ script.js
@@ -1,13 +1,30 @@
-const gameBoard = (function(){
-    let boardList = [];
-})();
+const gameBoard = function(){
+    let boardList = ['X', 'O', 'X', 'O', 'X', 'O', 'X', 'O'];
+    let gameOver = false;
 
-const displayController = (function() {
+    const render = function(){
 
-})();
+        const points = document.querySelectorAll('div.box');
+
+        points.forEach(point => point.addEventListener('click', pointClick))
+
+        const pointClick = function(){
+            this.textContent = "bob";
+        }
+    }
+
+}();
+
+const displayController = function() {
+
+}();
 
 const playerFactory = (name) => {
     const move = () => {};
 
     return { name, move}
 }
+
+const player = playerFactory('Bob');
+
+const computer = playerFactory('Computer');
